b8638a9ead7439df7038fead99cb3c7f2f39c658
berkbektas1 <berkbektas98@gmail.com>
2021-01-10 18:05:52 +0300

    invitation layout done

---
+++ app/src/main/java/com/example/meetingapp/activities/OutgoingInvitationActivity.java
@@ -0,0 +1,16 @@
+package com.example.meetingapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.meetingapp.R;
+
+public class OutgoingInvitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outgoing_invitation);
+    }
+}
